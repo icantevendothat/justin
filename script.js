7b5f10b40d8afe52130bb3085fcd34f703de5fb8
Annika Santhanam <aps659@nyu.edu>
2025-02-11 00:30:56 +0400

    correct format

--- script.js
+++ script.js
@@ -1,43 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const loadingScreen = document.getElementById('loading-screen');
+    const contentSection = document.querySelector('.content');
     const loadingElements = document.querySelectorAll('.loading-image, .loading-svg');
+    
+    contentSection.style.display = 'none';
+    
     let index = 0;
 
     const showNextElement = () => {
         if (index < loadingElements.length) {
-            loadingElements[index].classList.add('show');
+            const currentElement = loadingElements[index];
+            currentElement.classList.add('show'); 
+
+            if (currentElement.classList.contains('loading-svg')) {
+                const textElement = document.querySelector('.text');
+                textElement.style.opacity = 1;
+                textTypingEffect(textElement, "SOMETIMES I'LL BE \n DRAMATIC \n JUST LET ME BE \n DRAMATIC");
+            }
+
             index++;
 
-            setTimeout(showNextElement, 500);
+            setTimeout(showNextElement, 500); 
         } else {
-            const textElement = document.querySelector('.text');
-            textElement.style.opacity = 1; 
-            textTypingEffect(textElement, "SOMETIMES I'LL BE \n DRAMATIC \n JUST LET ME BE \n DRAMATIC");
+            setTimeout(() => {
+                contentSection.style.display = 'flex';
+
+                setTimeout(() => {
+                    window.scrollTo({
+                        top: window.innerHeight, 
+                        behavior: 'smooth'
+                    });
+                }, 500); 
+            }, 3000);
         }
     };
 
-    showNextElement();
+    showNextElement(); 
 
     function textTypingEffect(element, text, i = 0) {
         if (i === 0) {
-            element.textContent = "";
+            element.textContent = ""; 
         }
 
         element.textContent += text[i]; 
 
         if (i === text.length - 1) {
-            setTimeout(() => {
-                const contentElement = document.querySelector('.content');
-                contentElement.style.display = 'block'; 
-                window.scrollTo({
-                    top: contentElement.offsetTop, 
-                    behavior: 'smooth',
-                });
-
-                document.getElementById('loading-screen').style.display = 'none';
-            }, 500); 
             return; 
         }
 
-        setTimeout(() => textTypingEffect(element, text, i + 1), 50);
+        setTimeout(() => textTypingEffect(element, text, i + 1), 50); 
     }
 });
